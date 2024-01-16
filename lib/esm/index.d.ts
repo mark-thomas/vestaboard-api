f46d8c49d15351f836c1e7db425efe6069b1ee65
Mark Thomas <mark-thomas@users.noreply.github.com>
2024-01-16 12:47:48 -0800

    Build

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -1,7 +1,8 @@
 import VestaSubscription from './VestaSubscription';
 import VestaRW from './rwAPI';
 import VestaLocal from './localAPI';
-import { APIConfig, LocalAPIConfig, RWAPIConfig, SubscriptionAPIConfig, VestaboardControlMode, BoardCharArray, Line } from './types';
+import { APIConfig, LocalAPIConfig, RWAPIConfig, SubscriptionAPIConfig, VestaboardControlMode, BoardCharArray, Line, VestaboardLayout, VestaboardChar } from './types';
+import { createTransitionBoards } from './sharedFunctions';
 import { characterCode, characterCodeArray } from './values';
 export declare function createVestaboard(mode: VestaboardControlMode, config: APIConfig): VestaSubscription | VestaRW | VestaLocal;
-export { VestaSubscription, VestaRW, VestaLocal, VestaboardControlMode, characterCode, characterCodeArray, BoardCharArray, Line, APIConfig, SubscriptionAPIConfig, RWAPIConfig, LocalAPIConfig, };
+export { VestaSubscription, VestaRW, VestaLocal, VestaboardControlMode, characterCode, characterCodeArray, BoardCharArray, Line, APIConfig, SubscriptionAPIConfig, RWAPIConfig, LocalAPIConfig, VestaboardLayout, VestaboardChar, createTransitionBoards, };
